f69120acc8867776d950facb0ded86c94fcd847e
Nikita Grigorev <pettyaelse@gmail.com>
2026-09-07 19:25:54 +0700

    w

--- Compiler/IRGen.cs
+++ Compiler/IRGen.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace Compiler;
 
 public class IRGen
@@ -17,10 +19,52 @@ public class IRGen
     {
         List<IRFunction> functions = new();
 
+        foreach (FuncDecl funcDecl in unit.FuncDecls)
+        {
+            IRFunction func = GenFunction(funcDecl);
+            functions.Add(func);
+        }
+
         IRModule module = new()
         {
             Functions = functions,
         };
         return module;
+    }
+
+    private IRFunction GenFunction(FuncDecl funcDecl)
+    {
+        // TODO: Reuse lists
+        Debug.Assert(funcDecl.Symbol is { Type: FuncType });
+        FuncType funcType = (FuncType)funcDecl.Symbol.Type;
+
+        List<IRParam> irParams = new();
+        foreach (Param param in funcDecl.Params)
+        {
+            Debug.Assert(param.Type.ResolvedType != null);
+            IRParam p = new()
+            {
+                ParamType = param.Type.ResolvedType,
+                Index = irParams.Count,
+            };
+            irParams.Add(p);
+        }
+
+        List<StmtLet> locals = new();
+        CollectLocals(funcDecl.Body, locals);
+
+        List<IRBasicBlock> basicBlocks = new();
+
+        return new IRFunction
+        {
+            BasicBlocks = basicBlocks,
+            Name = funcDecl.Symbol.Name,
+            Params = irParams,
+            Type = funcType,
+        };
+    }
+
+    private void CollectLocals(Block body, List<StmtLet> locals)
+    {
     }
 }
