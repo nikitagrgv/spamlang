1ee79453cec27eaa1492b6e6cfa2865c90c17ac2
Nikita Grigorev <pettyaelse@gmail.com>
2026-09-11 00:00:04 +0700

    w

---
+++ research/c/spam.c
@@ -0,0 +1,14 @@
+
+int main()
+{
+    return 42;
+}
+
+int foo(int a)
+{
+    if (a == 888)
+    {
+        return 123;
+    }
+    return 456;
+}
---
+++ research/spam.asm
@@ -0,0 +1,60 @@
+    .intel_syntax noprefix
+
+    .data
+msg:
+    .asciz "hello, world!\n"
+
+    .text
+    .globl main
+main:
+    push rbp
+    mov rbp, rsp
+
+    # locals
+    sub rsp, 0x08
+    # arguments
+    sub rsp, 0x08
+    # align + shadow space
+    sub rsp, 0x00 + 0x20
+
+    # GetStdHandle
+    mov ecx, -11
+    call GetStdHandle
+
+    # WriteConsoleA
+    mov rcx, rax
+    lea rdx, [rip + msg]
+    mov r8d, 14
+    lea r9, [rbp-0x08]
+    mov qword ptr [rbp-0x10], 0
+    call WriteConsoleA
+
+    mov eax, [rbp-0x08]
+
+    mov rsp, rbp
+    pop rbp
+    ret
+
+# myprint(str: ptr, size: i32) -> i32
+myprint:
+	push rbp
+	mov rbp, rsp
+	
+	# align + shadow space for calls 
+	sub rsp, 0x00 + 0x20
+
+	# save args in OUR shadow space (shadow space: rbp+0x10...rbp+0x30)
+	mov qword ptr [rbp+0x10], rcx
+	mov dword ptr [rbp+0x18], rdx
+
+	mov ecx, -11
+	call GetStdHandle
+
+	mov rcx, rax
+	mov rdx, [rbp+0x10]
+	mov r8d, [rbp+0x18]
+	mov 
+
+	mov rsp, rbp
+	pop rbp
+	ret
