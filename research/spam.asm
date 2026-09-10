9cd821092e4e420d146eda321769eea94cec44a0
Nikita Grigorev <pettyaelse@gmail.com>
2026-09-11 00:34:25 +0700

    w

--- research/spam.asm
+++ research/spam.asm
@@ -40,7 +40,7 @@ myprint:
 	mov rdx, [rbp+0x10]
 	mov r8d, [rbp+0x18]
 	lea r9, [rbp+0x20]
-	mov qword ptr [rbp-0x08], 0
+	mov qword ptr [rbp-0x10], 0
 	call WriteConsoleA
 
 	# return num bytes written
