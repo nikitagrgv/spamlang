cd8c35432fea7ab171a7532567ae756721635169
Nikita Grigorev <pettyaelse@gmail.com>
2026-09-11 18:07:49 +0700

    w

--- Compiler/printers/MIRPrinter.cs
+++ Compiler/printers/MIRPrinter.cs
@@ -2,7 +2,20 @@ namespace Compiler.printers;
 
 public class MIRPrinter
 {
-    public static void Print(MModule module)
+    private StringWriter _writer;
+
+    public MIRPrinter(StringWriter writer)
+    {
+        _writer = writer;
+    }
+
+    public static void Print(MModule module, StringWriter writer)
+    {
+        MIRPrinter printer = new(writer);
+        printer.Print(module);
+    }
+
+    public void Print(MModule module)
     {
         foreach (MFunction func in module.Functions)
         {
@@ -10,23 +23,23 @@ public class MIRPrinter
         }
     }
 
-    private static void Print(MFunction func)
+    public void Print(MFunction func)
     {
-        Console.WriteLine("# -------------------------------------");
-        Console.WriteLine($"{func.Name}:");
+        _writer.WriteLine("# -------------------------------------");
+        _writer.WriteLine($"{func.Name}:");
         foreach (MBasicBlock bb in func.BasicBlocks)
         {
             Print(bb);
         }
 
-        Console.WriteLine();
+        _writer.WriteLine();
     }
 
-    private static void Print(MBasicBlock bb)
+    private void Print(MBasicBlock bb)
     {
         if (!string.IsNullOrEmpty(bb.Name))
         {
-            Console.WriteLine($"{bb.Name}:");
+            _writer.WriteLine($"{bb.Name}:");
         }
 
         foreach (MInstr instr in bb.Instructions)
@@ -35,9 +48,9 @@ public class MIRPrinter
         }
     }
 
-    private static void Print(MInstr instr)
+    private void Print(MInstr instr)
     {
-        Console.Write($"    {instr.Op.AsmName(),-5}");
+        _writer.Write($"    {instr.Op.AsmName(),-5}");
         if (instr.Left != null)
         {
             Print(instr.Left);
@@ -45,33 +58,33 @@ public class MIRPrinter
 
         if (instr.Right != null)
         {
-            Console.Write(", ");
+            _writer.Write(", ");
             Print(instr.Right);
         }
 
         if (!string.IsNullOrEmpty(instr.Comment))
         {
-            Console.Write($"              # {instr.Comment}");
+            _writer.Write($"              # {instr.Comment}");
         }
 
-        Console.WriteLine();
+        _writer.WriteLine();
     }
 
-    private static void Print(MOperand op)
+    private void Print(MOperand op)
     {
         switch (op)
         {
             case MOpImm mOpImm:
-                Console.Write(mOpImm.Value);
+                _writer.Write(mOpImm.Value);
                 break;
             case MOpLabel mOpLabel:
-                Console.Write(mOpLabel.Label);
+                _writer.Write(mOpLabel.Label);
                 break;
             case MOpReg mOpReg:
-                Console.Write(mOpReg.Reg.GetName(mOpReg.Size));
+                _writer.Write(mOpReg.Reg.GetName(mOpReg.Size));
                 break;
             case MOpMem mOpMem:
-                Console.Write($"[{mOpMem.Base.GetName(mOpMem.Size)}{mOpMem.Offset:+#,-#,0}]");
+                _writer.Write($"[{mOpMem.Base.GetName(mOpMem.Size)}{mOpMem.Offset:+#,-#,0}]");
                 break;
             default:
                 throw new ArgumentOutOfRangeException(nameof(op));
