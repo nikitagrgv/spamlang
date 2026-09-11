5151816af071bb354c92f258398bf4dca3bc70a9
Nikita Grigorev <pettyaelse@gmail.com>
2026-09-12 02:16:04 +0700

    w

--- Compiler/Codegen.cs
+++ Compiler/Codegen.cs
@@ -76,7 +76,11 @@ public class Codegen
 
         // TODO: Do this only if calls exists!
         int frameSize = curOffset;
-        frameSize += 0x20; // Shadow space
+        if (hasCalls)
+        {
+            frameSize += 0x20; // Shadow space
+        }
+
         frameSize = AlignTo(frameSize, 16); // ABI requirement
 
         // frame prologue
