6bbde8cdd2828d7caf90afbca93edb208e440d39
Nikita Grigorev <pettyaelse@gmail.com>
2026-09-11 17:48:37 +0700

    w

--- Compiler/printers/MIRPrinter.cs
+++ Compiler/printers/MIRPrinter.cs
@@ -4,5 +4,15 @@ public class MIRPrinter
 {
     public static void Print(MModule module)
     {
+        foreach (MFunction func in module.Functions)
+        {
+            Print(func);
+        }
+    }
+
+    private static void Print(MFunction func)
+    {
+        Console.WriteLine("# -------------------------------------");
+        Console.WriteLine($"{func.Name}:");
     }
 }
