3d3d0d10086a7e9bc719c46ee5b977e0051600da
Nikita Grigorev <pettyaelse@gmail.com>
2026-09-11 17:33:03 +0700

    w

---
+++ Compiler/MIRTypes.cs
@@ -0,0 +1,21 @@
+namespace Compiler;
+
+public abstract class MOperand;
+
+public sealed class MOpReg : MOperand
+{
+    public required Reg Reg { get; init; }
+    public required int Size { get; init; }
+}
+
+public sealed class MOpImm : MOperand
+{
+    public required Int128 Value { get; init; }
+}
+
+public sealed class MOpMem : MOperand
+{
+    public required Reg Base { get; init; }
+    public required int Offset { get; init; }
+    public required int Size { get; init; }
+}
