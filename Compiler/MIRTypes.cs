5c352995effb8f145fc5590de89e06e012dbdcaf
Nikita Grigorev <pettyaelse@gmail.com>
2026-09-12 17:50:54 +0700

    w

--- Compiler/MIRTypes.cs
+++ Compiler/MIRTypes.cs
@@ -45,6 +45,7 @@ public static class MOpcodes
 
 public abstract class MOperand;
 
+// TODO: Move these Size to instruction! 
 public sealed class MOpReg : MOperand
 {
     public required Reg Reg { get; init; }
