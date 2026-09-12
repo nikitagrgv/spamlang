81a440dd0256f1784db8c0ebbf1ee879b87f0668
Nikita Grigorev <pettyaelse@gmail.com>
2026-09-12 16:10:09 +0700

    w

--- Compiler/Codegen.cs
+++ Compiler/Codegen.cs
@@ -113,8 +113,11 @@ public class Codegen
             {
                 case IRConstantInt irConstantInt:
                 {
-                    MOpImm
-                    break;
+                    MOpImm op = new()
+                    {
+                        Value = irConstantInt.Value,
+                    };
+                    return op;
                 }
                 case IRFunction irFunction:
                 {
