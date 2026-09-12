4f49c028cd83cea6981a2fda418097f0a746d6c4
Nikita Grigorev <pettyaelse@gmail.com>
2026-09-12 15:15:27 +0700

    w

--- Compiler/Codegen.cs
+++ Compiler/Codegen.cs
@@ -208,7 +208,30 @@ public class Codegen
                         int leftOffset = instrIdToOffset[leftId];
                         int rightOffset = instrIdToOffset[rightId];
                         int instrOffset = instrIdToOffset[instrId];
-                        binary.Op
+                        MOpcode opcode;
+                        switch (binary.Op)
+                        {
+                            case IRBinaryOp.Add:
+                                opcode = MOpcode.Add;
+                                break;
+                            case IRBinaryOp.Sub:
+                                opcode = MOpcode.Sub;
+                                break;
+                            case IRBinaryOp.Mul:
+                                opcode = MOpcode.Mul;
+                                break;
+                            case IRBinaryOp.SDiv:
+                                throw new NotImplementedException();
+                            case IRBinaryOp.UDiv:
+                                throw new NotImplementedException();
+                            case IRBinaryOp.SRem:
+                                throw new NotImplementedException();
+                            case IRBinaryOp.URem:
+                                throw new NotImplementedException();
+                            default:
+                                throw new ArgumentOutOfRangeException();
+                        }
+
                         break;
                     }
                     case IRInstructionCall call:
--- Compiler/MIRTypes.cs
+++ Compiler/MIRTypes.cs
@@ -6,6 +6,8 @@ public enum MOpcode
     Add,
     Sub,
 
+    IMul,
+
     Dec,
     Inc,
 
