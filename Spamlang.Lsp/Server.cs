b4cfe857b51ea6a033765e128323245e839ca824
Nikita Grigorev <pettyaelse@gmail.com>
2026-09-16 01:59:11 +0700

    w

--- Spamlang.Lsp/Server.cs
+++ Spamlang.Lsp/Server.cs
@@ -1,9 +1,13 @@
+using System.Text;
+using System.Text.Json.Nodes;
+
 namespace Spamlang.Lsp;
 
 public class Server
 {
     private Stream _input;
     private Stream _output;
+    private bool _exit;
 
     public Server(Stream input, Stream output)
     {
@@ -12,6 +16,69 @@ public class Server
     }
 
     public void Run()
+    {
+        while (!_exit)
+        {
+            JsonNode? msg = ReadMessage();
+            if (msg == null)
+            {
+                break;
+            }
+
+            try
+            {
+                HandleMessage(msg);
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e);
+                if (msg["id"] is { } id && msg["method"] != null)
+                {
+                    const int internalErrorCode = -32603;
+                    ReplyError(id.DeepClone(), internalErrorCode, e.Message);
+                }
+            }
+        }
+    }
+
+    private void ReplyError(JsonNode id, int errorCode, string message)
+    {
+        JsonObject reply = new()
+        {
+            ["jsonrpc"] = "2.0",
+            ["id"] = id,
+            ["error"] = new JsonObject
+            {
+                ["code"] = errorCode,
+                ["message"] = message,
+            },
+        };
+        Send(reply);
+    }
+
+    private void Reply(JsonNode? id, JsonNode? result)
+    {
+        JsonObject reply = new()
+        {
+            ["jsonrpc"] = "2.0",
+            ["id"] = id,
+            ["result"] = result,
+        };
+    }
+
+    private void Send(JsonNode message)
+    {
+        byte[] body = Encoding.UTF8.GetBytes(message.ToJsonString());
+        _output.Write(Encoding.ASCII.GetBytes($"Content-Length: {body.Length}\r\n\r\n"));
+        _output.Write(body);
+        _output.Flush();
+    }
+
+    private JsonNode? ReadMessage()
+    {
+    }
+
+    private void HandleMessage(JsonNode message)
     {
     }
 }
