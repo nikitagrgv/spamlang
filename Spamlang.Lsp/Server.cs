58081d9867f1e6021c17bee2a57e550208e00c1b
Nikita Grigorev <pettyaelse@gmail.com>
2026-09-15 23:48:23 +0700

    w

--- Spamlang.Lsp/Server.cs
+++ Spamlang.Lsp/Server.cs
@@ -2,13 +2,16 @@ namespace Spamlang.Lsp;
 
 public class Server
 {
+    private Stream _input;
+    private Stream _output;
+
     public Server(Stream input, Stream output)
     {
-        
+        _input = input;
+        _output = output;
     }
 
     public void Run()
     {
-        
     }
 }
