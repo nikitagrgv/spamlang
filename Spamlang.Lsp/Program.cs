33558dffff419c3044f60ad6e10edbf4d3d36989
Nikita Grigorev <pettyaelse@gmail.com>
2026-09-15 23:47:37 +0700

    w

--- Spamlang.Lsp/Program.cs
+++ Spamlang.Lsp/Program.cs
@@ -2,7 +2,22 @@
 
 public class App
 {
-    public static void Main(string[] args)
+    public static int Main(string[] args)
     {
+        Stream input = new BufferedStream(Console.OpenStandardInput());
+        Stream output = Console.OpenStandardOutput();
+        Console.SetOut(Console.Error);
+
+        Server server = new(input, output);
+        try
+        {
+            server.Run();
+        }
+        catch (Exception)
+        {
+            return 1;
+        }
+
+        return 0;
     }
 }
---
+++ Spamlang.Lsp/Server.cs
@@ -0,0 +1,14 @@
+namespace Spamlang.Lsp;
+
+public class Server
+{
+    public Server(Stream input, Stream output)
+    {
+        
+    }
+
+    public void Run()
+    {
+        
+    }
+}
