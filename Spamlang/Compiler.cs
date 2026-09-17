d7599e2e563ac12cddbf7d6f7ab549113aa4884d
Nikita Grigorev <pettyaelse@gmail.com>
2026-09-17 20:52:23 +0700

    w

--- Spamlang/Compiler.cs
+++ Spamlang/Compiler.cs
@@ -78,7 +78,7 @@ public class Compiler
             return false;
         }
 
-        Backend.Backend.Result backendResult = Backend.Backend.Run(frontendResult.CompilationUnit, typeRegistry, timers);
+        Backend.Backend.Result backendResult = Backend.Backend.Run(frontendResult.TypedCompilationUnit, typeRegistry, timers);
 
         PrintIR(backendResult.IRModule);
         PrintMIR(backendResult.MModule);
