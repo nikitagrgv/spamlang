ac45c72fbebd8627d105038b2e7be253f0d24c89
Nikita Grigorev <pettyaelse@gmail.com>
2026-09-19 20:49:34 +0700

    w

--- Spamlang/DebugPrinters/HIRPrinter.cs
+++ Spamlang/DebugPrinters/HIRPrinter.cs
@@ -135,7 +135,7 @@ public class HIRPrinter
             prefix += "Symbol";
         }
 
-        Console.WriteLine($"{prefix}: {sym.Name}");
+        Console.WriteLine($"{prefix}: {sym.Name} ({sym.Type})");
     }
 
     private void PrintSymbols(int depth, IReadOnlyList<Symbol> symbols, string name = "")
