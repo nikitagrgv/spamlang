def0d02bfce5684e6cc0c9fb5e43883bd23da543
Nikita Grigorev <pettyaelse@gmail.com>
2026-09-20 23:16:10 +0700

    w

--- Spamlang.Frontend/Sema.cs
+++ Spamlang.Frontend/Sema.cs
@@ -1178,7 +1178,7 @@ public class Sema
 
             if (from.Kind == TypeKind.SignedInteger || from.Kind == TypeKind.UnsignedInteger)
             {
-                Debug.Assert(to.Size == 4 || to.Size == 8, "Condition below works only for f32/f64");
+                Debug.Assert(to.Size == 4 || to.Size == 8, "Fix condition below");
                 return to.Size > from.Size;
             }
 
